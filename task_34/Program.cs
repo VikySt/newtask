09f1d5e480686ccfa910cfded10d55a74eb52292
Виктория <vikystrelnikova@yandex.ru>
2022-10-20 21:20:35 +0500

    InputArray

---
+++ task_34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество
+
+void InputArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(100, 1000);
+}
+
+int a (int[] array)
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] % 2 == 0)
+         count = count + 1;
+   }
+
+   return count;
+}
+
+Console.Clear();
+int[] array = new int[7];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четных чисел в массиве равна: {a(array)}");
